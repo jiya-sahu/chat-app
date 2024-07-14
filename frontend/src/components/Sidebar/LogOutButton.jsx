c84f959dab2f1f719b508f8cddc3fe21dec173f1
jiya-sahu <jiyasahu135@gmail.com>
2024-07-14 12:46:24 +0530

    2nd part completed frontend ui design

---
+++ frontend/src/components/Sidebar/LogOutButton.jsx
@@ -0,0 +1,11 @@
+import { BiLogOut } from "react-icons/bi";
+import React from 'react'
+const LogOutButton = () => {
+  return (
+    <div className='mt-auto'>
+    <BiLogOut className='w-6 h-6 text-white cursor-pointer'/>
+    </div>
+  )
+}
+
+export default LogOutButton
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../../components/Sidebar/Sidebar.jsx'
+import MessageController from '../../components/messages/MessageContainer.jsx'
+const Home = () => {
+  return (
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <Sidebar/>
+      <MessageController/> 
+    </div>
+  )
+}
+
+export default Home
