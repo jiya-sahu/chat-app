2ab99b0a8b91e873acf9a572868a3decb3247e70
jiya-sahu <jiyasahu135@gmail.com>
2024-07-22 19:33:58 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import  dotenv from'dotenv';
 import cookieParser from 'cookie-parser';
-
+import path from 'path'
 
 import authroutes from './routes/auth.routes.js'
 import messageroutes from './routes/message.routes.js'
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT||5000;
 
 dotenv.config(); 
-
+const __dirname = path.resolve();
 app.use(express.json());//to parse incoming requests with json payloads
 app.use(cookieParser());
 
@@ -27,6 +27,12 @@ app.use("/api/auth/",authroutes);
 app.use("/api/message/",messageroutes);
 app.use("/api/users",userroutes)
 
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 app.listen(PORT,()=>{
     connectmongodb();
     console.log(`server running on port ${PORT}`);
